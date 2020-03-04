864cbcf590d4437999933b5974b18f73f8049196
emmanuelOnah <emmajerry2016@gmail.com>
2020-03-04 15:42:28 +0200

    Second commit of EuasPanky Software

--- src/scripts/adminpanel.js
+++ src/scripts/adminpanel.js
@@ -1,18 +1,20 @@
 (function() {
-  const adminpanelContainer = document.querySelector('.adminpanelContainer');
+  const studentsInfoContainer = document.querySelector(
+    '.studentsInfoContainer'
+  );
 
   const updateUi = (doc, id) => {
     const htmlTemplate = `
       <ul data-id="${id}" class="studentDetailsWrapper">
-<li class="type">Subission Type: ${doc.category} </li>
-<li class="departmen">Student Department: ${doc.department} </li>
-<li class="name">Submitted By: ${doc.name} </li>
-<li class="email">Student Email: ${doc.email} </li>
-<li class="message">Message: ${doc.message} </li>
-<li class="time">Submission Time: ${doc.created_at.toDate()} </li>
+          <li class="type">Subission Type: ${doc.category} </li>
+          <li class="departmen">Student Department: ${doc.department} </li>
+          <li class="name">Submitted By: ${doc.name} </li>
+          <li class="email">Student Email: ${doc.email} </li>
+          <li class="message">Message: ${doc.message} </li>
+          <li class="time">Submission Time: ${doc.created_at.toDate()} </li>
       </ul>`;
 
-    adminpanelContainer.innerHTML += htmlTemplate;
+    studentsInfoContainer.innerHTML += htmlTemplate;
   };
 
   const getStudentDetails = () => {
