ddaa1bb92a75c67f6afff79bf4d135f64f16f0bb
emmanuelOnah <emmajerry2016@gmail.com>
2020-03-24 01:17:59 +0200

    latest commit

--- src/scripts/adminpanel.js
+++ src/scripts/adminpanel.js
@@ -6,7 +6,7 @@
   const updateUi = (doc, id) => {
     const htmlTemplate = `
       <ul data-id="${id}" class="studentDetailsWrapper">
-          <li class="type">Subission Type: ${doc.category} </li>
+          <li class="type">Submission Type: ${doc.category} </li>
           <li class="departmen">Student Department: ${doc.department} </li>
           <li class="name">Submitted By: ${doc.name} </li>
           <li class="email">Student Email: ${doc.email} </li>
--- src/scripts/userform.js
+++ src/scripts/userform.js
@@ -1,4 +1,8 @@
 (function() {
+  /**
+   *
+   * @function{RESPONSIBLE FOR HANDLING FORM SUBMITION TO DB}
+   */
   const handleFormSumition = () => {
     const form = document.querySelector('form');
 
@@ -15,15 +19,19 @@
         created_at: firebase.firestore.Timestamp.fromDate(now)
       };
 
+      /**
+       *
+       * @function{RESPONSIBLE FOR SAVING VALUE TO LOCAL STORAGE}
+       */
+      localStorage.setItem('name', form.senderName.value);
+      localStorage.setItem(' email', form.senderEmail.value);
+      localStorage.setItem('department', form.department.value);
+      localStorage.setItem('category', form.category.value);
+
       db.collection('EuasPanky')
         .add(formData)
         .then(() => {
           console.log('SAVING PASSED');
-          console.log(
-            form.department.value,
-            form.senderName.value,
-            form.senderEmail.value
-          );
         })
         .catch(err =>
           console.log('SAVING FAILED' + '\n' + '*******' + err + '*******')
@@ -32,4 +40,21 @@
     });
   };
   handleFormSumition();
+
+  /**
+   *
+   * @function{RESPONSIBLE FOR GETTING VALUE FROM LOCAL STORAGE TO FORM FIELD}
+   */
+  const getValueFromLocalStorageToFormFields = () => {
+    const form = document.querySelector('form');
+    document.addEventListener('DOMContentLoaded', () => {
+      form.senderName.value = localStorage.getItem('name');
+      form.senderEmail.value = localStorage.getItem('email');
+      form.department.value = localStorage.getItem('department');
+      form.category.value = localStorage.getItem('category');
+
+      console.log(form.senderName.value = localStorage.getItem('name'))
+    });
+  };
+  getValueFromLocalStorageToFormFields();
 })();
